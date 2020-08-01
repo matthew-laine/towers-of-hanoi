4d431879546e4016491642c63f5e85e48953b734
Matt Laine <matt@Matts-MacBook-Pro.local>
2020-08-01 06:50:48 -0400

    refactor hanoi.hs to have more descriptive naming of the pegs from a, b, c to start, target, extra

--- hanoi.hs
+++ hanoi.hs
@@ -1,3 +1,3 @@
 hanoi :: Integer -> a -> a -> a -> [(a, a)]
 hanoi 0 _ _ _ = []
-hanoi n a b c = hanoi (n-1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
+hanoi n start target extra = hanoi (n-1) start extra target ++ [(start, target)] ++ hanoi (n-1) extra target start
